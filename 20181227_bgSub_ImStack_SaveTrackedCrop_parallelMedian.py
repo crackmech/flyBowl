a728dbf2079d2b364b49303c6ca5008f835c274c
crackmech <crack.mech@gmail.com>
2018-12-28 04:07:26 +0530

    trackFly_Kalman1

--- 20181227_bgSub_ImStack_SaveTrackedCrop_parallelMedian.py
+++ 20181227_bgSub_ImStack_SaveTrackedCrop_parallelMedian.py
@@ -15,36 +15,8 @@ import tkFileDialog as tkd
 import multiprocessing as mp
 import time
 import glob
-#from tracker_cm import Tracker
-##import sys
-#import matplotlib.pyplot as plt
-#import trackpy as tp
 import random
-import csv
 import itertools
-
-
-
-
-flyParams = cv2.SimpleBlobDetector_Params()
-flyParams.blobColor = 0
-flyParams.minThreshold = 5
-flyParams.maxThreshold = 240#120   120 for original image, 250 for bg subtracted images
-flyParams.filterByArea = True
-flyParams.filterByCircularity = True
-flyParams.minCircularity = 0
-flyParams.filterByConvexity = False
-flyParams.filterByInertia = False
-flyParams.minArea = 200# 200 for flyClimbing, 1000 for fly walking
-flyParams.maxArea = 8000
-
-# Create a detector with the parameters
-ver = (cv2.__version__).split('.')
-if int(ver[0]) < 3 :
-	detector = cv2.SimpleBlobDetector(flyParams)
-else : 
-	detector = cv2.SimpleBlobDetector_create(flyParams)
-
 
 
 nImThreshold = 0# if number of images in a folder is less than this, then the folder is not processed
@@ -79,171 +51,7 @@ def random_color():
     levels = range(0,255,32)
     return tuple(random.choice(levels) for _ in range(3))
 
-colors = [(0,200,200),(200,0,200),(200,200,0),(150,0,0),(0,0,200),(200,200,255)]
 colors = [random_color() for x in xrange(1000)]
-#csvOutFile = '/media/aman/data/thesis/colorPalette.csv'
-#with open(csvOutFile, "wb") as f:
-#    writer = csv.writer(f)
-#    writer.writerows(colors)
-
-def createTrack(trackData, img):
-    '''
-    input:
-        create an image of shape 'imgShape' with the x,y coordiates of the track from the array 'trackData
-    returns:
-        an np.array with the cv2 image array, which can be saved or viewed independently of this function
-    '''
-    #img = np.ones((imgShape[0], imgShape[1], 3), dtype = 'uint8')
-    blue = np.hstack((np.linspace(0, 255, num = len(trackData)/2),np.linspace(255, 0, num = (len(trackData)/2)+1)))
-    green = np.linspace(255, 0, num = len(trackData))
-    red = np.linspace(0, 255, num = len(trackData))
-    cv2.putText(img,'Total frames: '+str(len(trackData)), (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,255,255))
-    for i in xrange(1,len(trackData)):
-        cv2.circle(img,(int(trackData[i,0]), int(trackData[i,1])), 2, (blue[i], green[i], red[i]), thickness=2)#draw a circle on the detected body blobs
-    for i in xrange(1,len(trackData)):
-        if i%100==0:
-            cv2.putText(img,'^'+str(i), (int(trackData[i,0]), int(trackData[i,1])), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,255,255))
-    #cv2.imshow('track', img); cv2.waitKey(); cv2.destroyAllWindows()
-    return img
-
-
-
-def getTrackData(imStack, Blobparams, blurParams):
-    '''
-    returns the numpy array of coordinates of the centroid of blob in the stack of images provided as input numpy array 'imStack'
-    
-    '''
-    nFrames = imStack.shape[0]
-    ver = (cv2.__version__).split('.')
-    if int(ver[0]) < 3 :
-        	detector = cv2.SimpleBlobDetector(Blobparams)
-    else : 
-        	detector = cv2.SimpleBlobDetector_create(Blobparams)
-    trackData = np.zeros((nFrames,2))
-    kernel, sigma = blurParams
-    for f in xrange(nFrames):
-        im = imStack[f]
-        keypoints = detector.detect(cv2.GaussianBlur(im, (kernel, kernel), sigma))
-        kp = None
-        try:
-            for kp in keypoints:
-                trackData[f] = (kp.pt[0],kp.pt[1])
-        except:
-            pass
-    return trackData
-
-def getContours((idx, im, contourParams, blurParams)):
-    kernel, sigma = blurParams
-    ret,th = cv2.threshold(cv2.GaussianBlur(im, (kernel,kernel), sigma), contourParams['threshLow'], contourParams['threshHigh'],cv2.THRESH_BINARY)
-    th = cv2.bitwise_not(th)
-    ver = (cv2.__version__).split('.')
-    if int(ver[0]) < 3 :
-        contours, hierarchy = cv2.findContours(th, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-    else : 
-        im2, contours, hierarchy = cv2.findContours(th, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-    try:
-        contours = max(contours, key = cv2.contourArea)
-#        if not contourParams['minCntArea']<=cv2.contourArea(x)<=contourParams['maxCntArea']:
-#            contours = []
-    except:
-        contours = []
-    return [idx, contours]
-
-def getContourData(imStack, fList, contourParams, blurParams, pool):
-    '''
-    returns the ellipse fit data of the fly in the stack of images provided as input numpy array 'imStack'  
-    '''
-#    imgStack = np.array(pool.map(imRead, flist), dtype=np.uint8)
-#    poolArgList = itertools.izip(flist, itertools.repeat(params), np.arange(len(flist)))
-#    imgWithCnt = pool.map(imReadNCnt, poolArgList)
-    poolArgList = itertools.izip(fList, imStack, itertools.repeat(contourParams), itertools.repeat(blurParams))
-    contours = pool.map(getContours, poolArgList)
-    trackData = []
-    for idx,cnt in enumerate(contours):
-        if len(cnt[1])!=0:
-            try:
-                trackData.append([cnt[0], cv2.fitEllipse(cnt[1])])
-            except:
-                print ('no contour detected in frame# %s'%cnt[0])
-                trackData.append([cnt[0], 'noContourDetected'])
-        else:
-            print ('no contour detected in frame# %s'%cnt[0])
-            trackData.append([cnt[0], 'noContourDetected'])
-    return trackData
-#    trackData = []
-#    for idx, im in enumerate(imStack):
-#        frId = flist[idx]
-#        contours = getContours((frId, im, contourParams, blurParams))
-#        if len(contours[1])!=0:
-#            trackData.append([contours[0], cv2.fitEllipse(contours[1])])
-#        else:
-#            print ('no contour detected in frame# %d'%frId)
-#            trackData.append([contours[0], 'noContourDetected'])
-#    cv2.destroyAllWindows()
-#    return trackData
-
-def cropImstack(imStack, trackData, heightCropbox, widthCropbox, blurParams, ratTailParams):
-    '''
-    returns a list of all images, cropped as per cropBox dimensions
-    '''
-    kernel, sigma = blurParams
-    thresh, nIterations, erodeKernel = ratTailParams
-    ims = []
-    for i in xrange(imStack.shape[0]):
-        im = imStack[i]
-        try:
-            x,y = trackData[i]
-            if (heightCropbox<=y<=imStack.shape[1]-heightCropbox and widthCropbox<=x<=imStack.shape[2]-widthCropbox):
-                pts = [int(y)-heightCropbox, int(y)+heightCropbox, int(x)-widthCropbox,int(x)+widthCropbox]
-                im_cropped = im[pts[0]:pts[1], pts[2]:pts[3]]
-                _,th = cv2.threshold(cv2.GaussianBlur(im_cropped, (kernelSize,kernelSize), sigma), thresh, 255,cv2.THRESH_BINARY)
-                th = cv2.bitwise_not(th)
-                erosion = cv2.erode(th,erodeKernel,iterations = nIterations)
-                dilation = cv2.dilate(erosion, erodeKernel, iterations = nIterations)
-                ims.append([i,np.bitwise_xor(th, dilation)])
-            else:
-                 ims.append([i, 'NoCroppedImage'])
-        except:
-            pass
-    return ims
-
-def cropImstackGray(imStack, trackData, heightCropbox, widthCropbox):
-    '''
-    returns a list of all images, cropped as per cropBox dimensions
-    '''
-    ims = []
-    for i in xrange(imStack.shape[0]):
-        im = imStack[i]
-        try:
-            x,y = trackData[i]
-            if (heightCropbox<=y<=imStack.shape[1]-heightCropbox and widthCropbox<=x<=imStack.shape[2]-widthCropbox):
-                pts = [int(y)-heightCropbox, int(y)+heightCropbox, int(x)-widthCropbox,int(x)+widthCropbox]
-                im_cropped = im[pts[0]:pts[1], pts[2]:pts[3]]
-                ims.append([i,im_cropped])
-            else:
-                 ims.append([i, 'NoCroppedImage'])
-        except:
-            pass
-    return ims
-
-def saveCroppedIms(croppedStack, ImStack, saveDir, extension, hCropbox):
-    '''
-    saves the output of the tracked flies in the given format (specifice by 'extension') in the given directory.
-    If a fly is not detected in a continous frame, new folder is created to save the next sequence
-    '''
-    ext = extension
-    outDir = saveDir
-    cropDir = outDir+'_cropped/'
-    imDir = outDir+'_original_subIms/'
-    os.mkdir(imDir)
-    os.mkdir(cropDir)
-    for i in xrange(len(croppedStack)):
-        if 'NoCroppedImage' not in croppedStack[i][1]:
-            cv2.imwrite(cropDir+str(i)+ext, croppedStack[i][1])
-            cv2.imwrite(imDir+str(i)+ext, ImStack[i])
-        else:
-            print i, croppedStack[i][1]
-    return cropDir, imDir
 
 def getFiles(dirname, extList):
     filesList = []
@@ -261,7 +69,6 @@ def displayImgs(imgs, fps):
 
 def imRead(x):
     return cv2.imread(x, cv2.IMREAD_GRAYSCALE)
-    #return cv2.rotate(cv2.imread(x, cv2.IMREAD_GRAYSCALE), cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 def getBgIm(imgs):
     '''
@@ -270,51 +77,126 @@ def getBgIm(imgs):
     avg = np.array((np.median(imgs, axis=0)))
     return cv2.convertScaleAbs(avg)
 
-def getBgSubImStack((inImgstack, bgIm)):
-    '''
-    returns the stack of images after subtracting the background image from the input imagestack
-    '''
-    subIms = np.zeros(np.shape(inImgstack), dtype=np.uint8)
-    for f in range(0, len(inImgstack)):
-        subIms[f] = cv2.bitwise_not(cv2.absdiff(inImgstack[f], bgIm))
-    return subIms
-    
 def getBgSubIm((inImg, bgIm)):
     '''
     returns the stack of images after subtracting the background image from the input imagestack
     '''
     return cv2.bitwise_not(cv2.absdiff(inImg, bgIm))    
     
-def getSubIms(dirname, imExts, pool, workers):
+
+def getImstkBgIm(dirname, imExts, imNThresh, pool, workers):
     '''
     tracks the fly using cv2.SimpleBlobDetector method and saves the tracked flies in folders
     '''
-    flist = getFiles(dirname, imExts)[:1024]
+    flist = getFiles(dirname, imExts)
     nImsToProcess = len(flist)
-    print 'processing %i frames in\n==> %s'%(nImsToProcess, dirname)
     startTime = time.time()
-    imgStack = np.array(pool.map(imRead, flist), dtype=np.uint8)
+    print 'processing %i frames in\n==> %s'%(nImsToProcess, dirname)
+    imWidth, imHeight = imRead(flist[0]).shape
+    imgStack = np.zeros((len(flist), imWidth, imHeight), dtype=np.uint8)
+    for i in xrange(0, len(flist), imNThresh):
+        if i+imNThresh>len(flist):
+            j = len(flist)
+        else:
+            j = i+imNThresh
+        imgStack[i:j] = np.array(pool.map(imRead, flist[i:j]), dtype=np.uint8)
     t1 = time.time()-startTime
-    print("imRead time for %d frames: %s Seconds at %f FPS\n"%(len(flist),t1 ,len(flist)/float(t1)))
-    displayImgs(imgStack, 200)
+    print("imRead time for %d frames: %0.2f Seconds at %0.2f FPS"%(len(flist),t1 ,len(flist)/float(t1)))
     t1 = time.time()
-    imStackChunks = np.array_split(imgStack, 4*workers, axis=1)
-    #imStackChunks = [x.copy() for x in imStackChunks if x.size > 0]
+    imStackChunks = np.array_split(imgStack[5:1000], 4*workers, axis=1)
     bgImChunks = pool.map(getBgIm, imStackChunks)
     bgIm = np.array(np.vstack((bgImChunks)), dtype=np.uint8)
     t2 = time.time()-t1
-    print("parallel bg calculation time for %d frames: %s Seconds at %f FPS\n"%(len(flist),t2 ,len(flist)/float(t2)))
+    print("parallel bg calculation time for %d frames: %0.2f Seconds at %0.2f FPS\n"%(len(flist),t2 ,len(flist)/float(t2)))
     t2 = time.time()
-    subIms = np.array(pool.map(getBgSubIm, itertools.izip(imgStack, itertools.repeat(bgIm))), dtype=np.uint8)
-    t = time.time()-t2
-    print("bg Subtraction time for %d frames: %s Seconds at %f FPS\n"%(len(flist),t ,len(flist)/float(t)))
-    t = time.time()-t1
-    print("imRead and bgSub time for %d frames: %s Seconds at %f FPS\n"%(len(flist),t ,len(flist)/float(t)))
-    displayImgs(subIms, 200)
-    return imgStack, subIms, flist
+    pool.close()
+    return imgStack, bgIm
 
 
- 
+from detectors import Detectors
+from tracker import Tracker
+def main():
+    """Main function for multi object tracking
+    """
+
+    # Create Object Detector
+    detector = Detectors()
+
+    # Create Object Tracker
+    """Initialize variable used by Tracker class
+    Args:
+        dist_thresh: distance threshold. When exceeds the threshold,
+                     track will be deleted and new track is created
+        max_frames_to_skip: maximum allowed frames to be skipped for
+                            the track object undetected
+        max_trace_length: trace path history length
+        trackIdCount: identification of each track object
+    Return:
+        None
+    """
+    tracker = Tracker(200, 50, 25, 100)
+    
+    # Variables initialization
+    pause = False
+    track_colors = [random_color() for x in xrange(1000)]
+    # Infinite loop to process video frames
+    fps = 0
+    stTm = time.time()
+    for fr in xrange(len(flyContours[0])):
+        # Capture frame-by-frame
+        frame = getBgSubIm((flyContours[0][fr], flyContours[1]))
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        outFrame = cv2.cvtColor(flyContours[0][fr], cv2.COLOR_GRAY2BGR)
+
+        # Detect and return centeroids of the objects in the frame
+        centers = detector.DetectCM(frame)
+        # If centroids are detected then track them
+        if (len(centers) > 0):
+
+            # Track object using Kalman Filter
+            tracker.Update(centers)
+
+            # For identified object tracks draw tracking line
+            # Use various colors to indicate different track_id
+            for i in range(len(tracker.tracks)):
+                if (len(tracker.tracks[i].trace) > 1):
+                    for j in range(len(tracker.tracks[i].trace)-1):
+                        # Draw trace line
+                        x1 = tracker.tracks[i].trace[j][0][0]
+                        y1 = tracker.tracks[i].trace[j][1][0]
+                        x2 = tracker.tracks[i].trace[j+1][0][0]
+                        y2 = tracker.tracks[i].trace[j+1][1][0]
+                        clr = tracker.tracks[i].track_id
+#                        cv2.line(outFrame, (int(x1), int(y1)), (int(x2), int(y2)),
+#                                 track_colors[clr], 2)
+#                        cv2.circle(outFrame, (int(x1), int(y1)), 5, (255,25,255), 2)
+                    cv2.circle(outFrame, (int(x2), int(y2)), 5, track_colors[clr], 5)
+
+            # Display the resulting tracking frame
+            cv2.imshow('Tracking', outFrame)
+
+        # Check for key strokes
+        k = cv2.waitKey(1) & 0xff
+#        if k == 27:  # 'esc' key has been pressed, exit program.
+#            break
+#        if k == 112:  # 'p' has been pressed. this will pause/resume the code.
+#            pause = not pause
+#            if (pause is True):
+#                print("Code is paused. Press 'p' to resume..")
+#                while (pause is True):
+#                    # stay in this loop until
+#                    key = cv2.waitKey(1) & 0xff
+#                    if key == 112:
+#                        pause = False
+#                        print("Resume code..!!")
+#                        break
+#        tm = time.time()
+#        fps= (fps+(1.0/(tm-stTm)))/2
+#        print('FPS: %0.3f (frame# %d)'%(fps, fr))#(1.0/(tm-stTm)))
+#        stTm = tm
+    cv2.destroyAllWindows()
+    print('Tracking average FPS: %0.3f'%(float(fr)/(time.time()-stTm)))#(1.0/(tm-stTm)))
+
 
 
 
@@ -323,101 +205,24 @@ imDataFolder = 'imageData'
 statsfName = 'contoursStats_threshBinary'
 statsFileHeader = ['frameDetails','x-coord','y-coord','minorAxis (px)','majorAxis (px)','angle','area (px)']
 
-nThreads = 4
+imNThresh = 4096
+nThreads = 8
 pool = mp.Pool(processes=nThreads)
 
 imFolder = '/media/aman/data/KCNJ10/KCNJ10_labeledVideos/fullRes/20180927_000154_20180922_2230_KCNJ10SS--flyBowl/imageData/20180927_000433'
-flyContours = getSubIms(imFolder, imExtensions, pool, nThreads)
 
 
-
-
-
-
-""" 
-
-initialDir = '/media/pointgrey/data/flywalk/'
-initialDir = '/media/aman/data/flyWalk_data/climbingData/gait/data/tmp/pythonTmp/'
-initialDir = '/media/aman/data/flyWalk_data/climbingData/gait/data/copiedLegTrackingTrackData/'
-baseDir = getFolder(initialDir)
-
-outDir = '/media/aman/data/flyWalk_data/climbingData/gait/data/tmp/'
-
-imExtensions = ['*.png', '*.jpeg']
-heightCrop = 80
-widthCrop = 100
-legCntThresh = 0.31
-
-nThreads = 4
-kernelSize = 5
-gauBlurParams = (kernelSize,1)
-
-threshVal = 250
-nIterations = 2
-kernel = np.ones((kernelSize,kernelSize),np.uint8)
-
-pxMvdByLegBwFrm = 50
-legTipFrmSkipthresh = 40
-
-rattailparams = (threshVal, nIterations, kernel)
-#baseDir = initialDir
-print baseDir
-cntParams = {'maxCntArea'   :   7000,\
-             'minCntArea'   :   1000,\
-             'threshLow'    :   230,\
-             'threshHigh'   :   255}
-
-
-trackparams = [imExtensions, heightCrop, widthCrop, cntParams, flyParams,\
-               nImThreshold, gauBlurParams, rattailparams]
-
-
-rawDirs = getDirList(baseDir)
-pool = mp.Pool(processes=nThreads)
-procStartTime = time.time()
-totalNFrames = 0
-print "Started processing directories at "+present_time()
-for _,rawDir in enumerate(rawDirs):
-    d = os.path.join(rawDir, imgDatafolder)
-    print rawDir
-    imdirs = getDirList(d)
-    for imdir in imdirs:
-        startTime = time.time()
-        nFrames = len(getFiles(imdir, imExtensions))
-        fname = imdir.rstrip(os.sep)+'_legTips-Climbing_allPts_'
-        legTipLocs = getAllLocs(imdir, trackparams, legCntThresh, fname, pool, nThreads)
-        print('Processed %i frames in %0.3f seconds\nAverage total processing speed: %05f FPS'\
-        %(nFrames, time.time()-startTime, (nFrames/(time.time()-startTime)))) 
-        totalNFrames +=nFrames
+flyContours = getImstkBgIm(imFolder, imExtensions, imNThresh, pool, nThreads)
 pool.close()
-totSecs = time.time()-procStartTime
-print('Processing finished at: %05s, in $sSeconds, total processing speed: %05f FPS'\
-      %(present_time(),totSecs , totalNFrames/totSecs))
+cv2.imshow('123', flyContours[1])
+cv2.waitKey()
+cv2.destroyAllWindows()
 
+main()
 
+"""FIX TRACKS WITH NO FLIES BY CROSS CORRELATING FLY CENTROIND DETECTION WITH TRACKING DATA"""
 
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#
+#im = np.vstack(flyContours)
+#
 
---
+++ detectors.py
@@ -0,0 +1,158 @@
+'''
+    File name         : detectors.py
+    File Description  : Detect objects in video frame
+    Author            : Srini Ananthakrishnan
+    Date created      : 07/14/2017
+    Date last modified: 07/16/2017
+    Python Version    : 2.7
+'''
+
+# Import python libraries
+import numpy as np
+import cv2
+
+# set to 1 for pipeline images
+debug = 0
+
+
+class Detectors(object):
+    """Detectors class to detect objects in video frame
+    Attributes:
+        None
+    """
+    def __init__(self):
+        """Initialize variables used by Detectors class
+        Args:
+            None
+        Return:
+            None
+        """
+        self.fgbg = cv2.createBackgroundSubtractorMOG2()
+
+    def Detect(self, frame):
+        """Detect objects in video frame using following pipeline
+            - Convert captured frame from BGR to GRAY
+            - Perform Background Subtraction
+            - Detect edges using Canny Edge Detection
+              http://docs.opencv.org/trunk/da/d22/tutorial_py_canny.html
+            - Retain only edges within the threshold
+            - Find contours
+            - Find centroids for each valid contours
+        Args:
+            frame: single video frame
+        Return:
+            centers: vector of object centroids in a frame
+        """
+
+        # Convert BGR to GRAY
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        if (debug == 1):
+            cv2.imshow('gray', gray)
+
+        # Perform Background Subtraction
+        fgmask = gray#self.fgbg.apply(gray)
+
+        if (debug == 0):
+            cv2.imshow('bgsub', fgmask)
+
+        # Detect edges
+        edges = cv2.Canny(fgmask, 50, 190, 3)
+
+        if (debug == 1):
+            cv2.imshow('Edges', edges)
+
+        # Retain only edges within the threshold
+        ret, thresh = cv2.threshold(edges, 127, 255, 0)
+
+        # Find contours
+        _, contours, hierarchy = cv2.findContours(thresh,
+                                                  cv2.RETR_EXTERNAL,
+                                                  cv2.CHAIN_APPROX_SIMPLE)
+
+        if (debug == 0):
+            cv2.imshow('thresh', thresh)
+
+        centers = []  # vector of object centroids in a frame
+        # we only care about centroids with size of bug in this example
+        # recommended to be tunned based on expected object size for
+        # improved performance
+        blob_radius_thresh_min = 12
+        blob_radius_thresh_max = 25
+        # Find centroid for each valid contours
+        for cnt in contours:
+            try:
+                # Calculate and draw circle
+                (x, y), radius = cv2.minEnclosingCircle(cnt)
+                centeroid = (int(x), int(y))
+                radius = int(radius)
+                if (blob_radius_thresh_min< radius < blob_radius_thresh_max):
+                    cv2.circle(frame, centeroid, radius, (0, 255, 0), 2)
+                    b = np.array([[x], [y]])
+                    centers.append(np.round(b))
+            except ZeroDivisionError:
+                pass
+
+        # show contours of tracking objects
+        cv2.imshow('Track Bugs', frame)
+
+        return centers
+
+
+    def DetectCM(self, frame):
+        """Detect objects in video frame using following pipeline
+            - Convert captured frame from BGR to GRAY
+            - Perform Background Subtraction
+            - Detect edges using Canny Edge Detection
+              http://docs.opencv.org/trunk/da/d22/tutorial_py_canny.html
+            - Retain only edges within the threshold
+            - Find contours
+            - Find centroids for each valid contours
+        Args:
+            frame: single video frame
+        Return:
+            centers: vector of object centroids in a frame
+        """
+
+        # Convert BGR to GRAY
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+        # Retain only edges within the threshold
+        ret, thresh = cv2.threshold(gray, 200, 255, 0)
+        thresh = cv2.bitwise_not(thresh)
+        # Find contours
+        _, contours, hierarchy = cv2.findContours(thresh,
+                                                  cv2.RETR_EXTERNAL,
+                                                  cv2.CHAIN_APPROX_SIMPLE)
+        kernelSize = 5
+        nIterations = 2
+        erodeKernel = np.ones((kernelSize,kernelSize),np.uint8)
+        thresh = cv2.erode(thresh,erodeKernel,iterations = nIterations)
+#        if (debug == 0):
+#            cv2.imshow('thresh', thresh)
+
+        centers = []  # vector of object centroids in a frame
+        # we only care about centroids with size of bug in this example
+        # recommended to be tunned based on expected object size for
+        # improved performance
+        blob_radius_thresh_min = 7
+        blob_radius_thresh_max = 30
+        # Find centroid for each valid contours
+        for cnt in contours:
+            try:
+                # Calculate and draw circle
+                (x, y), radius = cv2.minEnclosingCircle(cnt)
+                centeroid = (int(x), int(y))
+                radius = int(radius)
+                if (blob_radius_thresh_min< radius < blob_radius_thresh_max):
+                    cv2.circle(frame, centeroid, radius, (0, 255, 0), 2)
+                    b = np.array([[x], [y]])
+                    centers.append(np.round(b))
+            except ZeroDivisionError:
+                pass
+
+        # show contours of tracking objects
+        # cv2.imshow('Track Bugs', frame)
+
+        return centers
